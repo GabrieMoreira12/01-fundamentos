190028d58979118e84fc873830bf645a51edd20d
Andrew <asalazar@solucionesun.com>
2025-02-02 16:06:51 -0600

    se ah relizado ejemolos de alertas y comunicacion del app js y comunicaciones de rutas en index html

---
+++ assets/js/alerts.js
@@ -0,0 +1,14 @@
+
+
+
+alert('HOLA MUNDO');
+
+
+let nombre = prompt('¿cual es tu nombre?')
+console.log( nombre );
+console.log( '****' + nombre +'****' );
+
+const seleccion = confirm('¿esta seguro de borrar esto?');
+console.log(seleccion);
+
+//console.log ( global );//
---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+console.log (miMuevoNombre);
+alert( "hola yelowwwww")
+let a = 10,
+    b = 20,
+    c = 'Hola ' ,
+    d = 'Tony star',
+    x = a + b;
+
+const saludo = c + d;
+
+
+
+
+
+
+
+
+
+
+
+c = 'hola de nuevo';
+
+
+
+
+let  miNombre = 'GABRIEL MOREIRA';
+
+///let miMuevoNombre = 'Gabriel Moreira';
